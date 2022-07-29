84e7655ff5cd8cd6098a88f769b27d466231d693
Palash Bauri <palashbauri1@gmail.com>
2022-07-30 02:59:04 +0530

    july 30, 22

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,11 @@ fish_add_path $HOME/meh
 set --global GPG_TTY (tty)
 set --global hydro_symbol_prompt 🐨
 set --global OOO_FORCE_DESKTOP "gnome" libreoffice
+
+set --global GTK_IM_MODULE ibus
+set --global QT_IM_MODULE ibus
+set --global XMODIFIERS @im=ibus
+alias vim nvim
+alias v nvim
+fish_add_path /usr/local/go/bin
+fish_add_path $HOME/go/bin

fc345a2ff934bd6530688841005d37e20c32e413
Palash Bauri <palashbauri1@gmail.com>
2022-08-01 19:08:00 +0530

    aug 1, 2022

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,18 @@ set --global OOO_FORCE_DESKTOP "gnome" libreoffice
 set --global GTK_IM_MODULE ibus
 set --global QT_IM_MODULE ibus
 set --global XMODIFIERS @im=ibus
+
+
 alias vim nvim
 alias v nvim
+alias sshh "kitty +kitten ssh"
+
 fish_add_path /usr/local/go/bin
 fish_add_path $HOME/go/bin
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/palash/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+

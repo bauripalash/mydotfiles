9976ef5fe20cc163e58164894992c833a7daf441
Palash Bauri <palashbauri1@gmail.com>
2022-11-24 20:57:35 +0530

    Nov24-2022-2

--- .config/nvim/lua/vim.lua
+++ .config/nvim/lua/vim.lua
@@ -45,13 +45,10 @@ end)
 
 
 local function map(mode , lhs , rhs , opts)
-
 	local options = { noremap = true }
-
 	if opts then
 		options = vim.tbl_extend("force" , options , opts)
 	end
-
 	api.nvim_set_keymap(mode , lhs , rhs , options)
 end
 
